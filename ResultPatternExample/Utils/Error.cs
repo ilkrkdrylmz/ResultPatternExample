5892c8d252bb6aef54e11d6ee60f92d079ada707
İlker Kadir YILMAZ <i.k.y@hotmail.com>
2024-11-08 12:28:17 +0300

    Add project files.

---
+++ ResultPatternExample/Utils/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace ResultPatternExample.Utils
+{
+    public sealed record Error(int Code, string Description)
+    {
+        public static Error ProductNotFound => new(100, "Product not found");
+
+        public static Error ProductBadRequest => new(101, "Product bad request");
+    }
+}
---
+++ ResultPatternExample/Utils/Result.cs
@@ -0,0 +1,19 @@
+﻿namespace ResultPatternExample.Utils
+{
+    public sealed class CsoftResult<T>
+    {
+        public T? Value { get; }
+        public Error? Error { get; }
+        public bool IsError => Error != null;
+
+        private CsoftResult(T value)
+        {
+            Value = value ?? throw new ArgumentNullException(nameof(value), "Value cannot be null.");
+            Error = null;
+        }
+
+        private CsoftResult(Error error) => Error = error ?? throw new ArgumentNullException(nameof(error), "Error cannot be null.");
+        public static CsoftResult<T> Success(T value) => new(value);
+        public static CsoftResult<T> Failure(Error error) => new(error);
+    }
+}
